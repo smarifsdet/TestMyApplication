b5c8fb39a1cb2e90fdf26ea24b2900256a948be2
Hasan <smarif.sdet@gmail.com>
2023-01-14 15:29:50 -0500

    Fix the issue

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"6a198a79-7669-44ed-bc16-322ce5f4db89","feature":"Functional Scenarios for ASK Login and Logout","scenario":"Validation of labels in Create Request page in ASK application","start":1673649219502,"group":1,"content":"","tags":"@test,","end":1673649238737,"className":"failed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"af767a25-7a4b-49d5-8be3-262e164cad07","feature":"Functional Scenarios for ASK Login and Logout","scenario":"Validation of labels in Create Request page in ASK application","start":1673727720765,"group":1,"content":"","tags":"@test,","end":1673727764476,"className":"failed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
 });
